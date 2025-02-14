08442504384708104dc95ee1c33997e2132516ec
Melvin Sarabia <jmelvinsarabia032@gmail.com>
2025-02-14 18:04:18 +0800

    Added more intermediate transformations

--- dags/transform/models/intermediate/int_products.sql
+++ dags/transform/models/intermediate/int_products.sql
@@ -1,2 +1,25 @@
-SELECT sp.* 
-FROM {{ ref('stg_products') }} AS sp
+WITH clean_cols AS 
+    (
+    SELECT
+        GOODS_TITLE_LINK_JUMP,
+        SPLIT("FILE_NAME", '-')[2] AS "category",
+        SPLIT(LTRIM(RANK_TITLE, '#'), ' ')[0]::INTEGER AS rank_title,
+        LTRIM(INITCAP(RANK_SUB), 'In ') as rank_sub,
+        PRICE,
+        DISCOUNT,
+        CASE
+            WHEN LEFT(selling_proposition, LENGTH(selling_proposition) - 15) LIKE '%k%' 
+                THEN LEFT(selling_proposition, LENGTH(selling_proposition) - 16)::FLOAT * 1000
+            ELSE LEFT(selling_proposition, LENGTH(selling_proposition) - 15)::FLOAT
+        END AS selling,
+        selling_proposition,
+        COLOR_COUNT,
+        BLACKFRIDAYBELTS_CONTENT
+    FROM {{ ref('stg_products') }}
+    )
+
+
+SELECT 
+    DISTINCT * 
+FROM clean_cols
+    
--- dags/transform/target/run/transform/models/intermediate/int_products.sql
+++ dags/transform/target/run/transform/models/intermediate/int_products.sql
@@ -4,8 +4,30 @@
 
         create or replace transient table E_COMMERCE_PRODUCT.products_schema_intermediate.int_products
          as
-        (SELECT sp.* 
-FROM E_COMMERCE_PRODUCT.products_schema.stg_products AS sp
+        (WITH clean_cols AS 
+    (
+    SELECT
+        GOODS_TITLE_LINK_JUMP,
+        SPLIT("FILE_NAME", '-')[2] AS "category",
+        SPLIT(LTRIM(RANK_TITLE, '#'), ' ')[0]::INTEGER AS rank_title,
+        LTRIM(INITCAP(RANK_SUB), 'In ') as rank_sub,
+        PRICE,
+        DISCOUNT,
+        CASE
+            WHEN LEFT(selling_proposition, LENGTH(selling_proposition) - 15) LIKE '%k%' 
+                THEN LEFT(selling_proposition, LENGTH(selling_proposition) - 16)::FLOAT * 1000
+            ELSE LEFT(selling_proposition, LENGTH(selling_proposition) - 15)::FLOAT
+        END AS selling,
+        selling_proposition,
+        COLOR_COUNT,
+        BLACKFRIDAYBELTS_CONTENT
+    FROM E_COMMERCE_PRODUCT.products_schema.stg_products
+    )
+
+
+SELECT 
+    DISTINCT * 
+FROM clean_cols
         );
       
   
